12e2dff82856fb5b7262717e2a7e214c10d3844d
Saswat <saswatsubhajyotimallick@gmail.com>
2020-07-13 20:26:47 +0530

    bug fixes

--- demo.py
+++ demo.py
@@ -1,11 +1,11 @@
 from character_segmentation import segment_characters
 from plate_detection import find_contours
 from license_plate_extraction import extract_plate
-# from CNN_model import CNN_Model
+from results import show_results
+
 import cv2
 import numpy as np
 import keras
-import matplotlib.pyplot as plt
 
 original_image = cv2.imread('test.jpg')
 plate_img, plate = extract_plate(original_image)
@@ -18,29 +18,5 @@ char_list = find_contours(dimensions, img_dilate)
 #     cv2.waitKey(0)
 
 model = keras.models.load_model('model.h5')
-def fix_dimension(img): 
-  new_img = np.zeros((28,28,3))
-  for i in range(3):
-    new_img[:,:,i] = img
-  return new_img
-  
-def show_results():
-    dic = {}
-    characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    for i,c in enumerate(characters):
-        dic[i] = c
 
-    output = []
-    for i,ch in enumerate(char_list): #iterating over the characters
-        img_ = cv2.resize(ch, (28,28))
-        img = fix_dimension(img_)
-        img = img.reshape(1,28,28,3) #preparing image for the model
-        y_ = model.predict_classes(img)[0] #predicting the class
-        character = dic[y_] #
-        output.append(character) #storing the result in a list
-        
-    plate_number = ''.join(output)
-    
-    return plate_number
-
-print(show_results())
+print(show_results(model, char_list))
